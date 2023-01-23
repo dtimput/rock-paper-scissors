5b413a9e664a4bb56a9e81aa5244c2b3c8a54df3
Dylan <122069647+dtimput@users.noreply.github.com>
2023-01-23 10:51:05 -0800

    improved game logic, to end after 5 rounds

--- script.js
+++ script.js
@@ -25,43 +25,35 @@ function playRound(playerChoice, computerChoice){
 
     //checks for tie and invalid choices
     if(playerChoice == computerChoice){
-        scored = false;
         return `You both picked ${playerChoice}! Draw!`;
     }else if(playerChoice != 'rock' && playerChoice && playerChoice != 'paper' && playerChoice != 'scissors'){
-        scored = false;
         return 'You can only choose "rock", "paper", or "scissors"!';
     }
     //compares different scenarios
     switch(playerChoice){
         case 'rock':
             if(computerChoice == 'paper'){
-                scored = true;
                 computerScore += 1;
                 return 'You lose! Paper beats Rock!';
             }else{
-                scored = true;
                 playerScore += 1;
                 return 'You win! Rock beats Scissors!';
             }
             break;
         case 'paper':
             if(computerChoice == 'scissors'){
-                scored = true;
                 computerScore += 1;
                 return 'You lose! Scissors beats Paper!';
             }else{
-                scored = true;
                 playerScore += 1;
                 return 'You win! Paper beats Rock!';
             }
             break;
         case 'scissors':
             if(computerChoice == 'rock'){
-                scored = true;
                 computerScore += 1;
                 return 'You lose! Rock beats Scissors!';
             }else{
-                scored = true;
                 playerScore += 1;
                 return 'You win! Scissors beats Paper!';
             }
@@ -72,9 +64,8 @@ function playRound(playerChoice, computerChoice){
 }
 
 function game(){
-    for (let i = 0; i < 5; i++) {
-        if(scored == false){
-            i--;
+    for (let i = 0; i < 50; i++) {
+        if(playerScore + computerScore < 5){
             const yourChoice = prompt("Choose Rock Paper or Scissors!").toLowerCase();
             const computerChoice = getComputerChoice();
 
@@ -85,15 +76,7 @@ function game(){
             console.log('Your score: ', playerScore);
             console.log('Computers score: ', computerScore);
         }else{
-            const yourChoice = prompt("Choose Rock Paper or Scissors!").toLowerCase();
-            const computerChoice = getComputerChoice();
-
-            console.log('Your choice: ', yourChoice);  
-            console.log('The computers choice: ', computerChoice);
-            console.log(playRound(yourChoice, computerChoice));
-
-            console.log('Your score: ', playerScore);
-            console.log('Computers score: ', computerScore);
+            break;
         }
 
     }
